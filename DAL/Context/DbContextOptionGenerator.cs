f431b50fb83331dd4fca55ebcf7148c063f848c4
Emin Sinan Tokyay <32817454+ESTokyay@users.noreply.github.com>
2023-04-24 20:24:58 +0300

    background servis
    Her durdurup başlatıldığında aynı veri eklenmesi engellendi.

---
+++ DAL/Context/DbContextOptionGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL.Context
+{
+    public class DbContextOptionGenerator
+    {
+        public static DbContextOptions<MovieContext> BuildOptions(IConfiguration _conf)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../MovieSuggApp"))
+                .AddJsonFile("appsettings.Development.json")
+                .Build();            
+            var connString = configuration.GetSection("MovieSuggDb").GetSection("ConnParam").Value;
+
+            var builder = new DbContextOptionsBuilder<MovieContext>();
+            builder.UseSqlServer(connString);
+            return builder.Options;
+            
+        }
+    }
+}
--- DAL/Worker/MovieDetailsProcessor.cs
+++ DAL/Worker/MovieDetailsProcessor.cs
@@ -15,10 +15,12 @@ namespace DAL.Worker
     {
         private readonly IConfiguration _config;
         private readonly string filmlerUrl;
+        private DbContextOptions<MovieContext> _dbContextOptions;
         public MovieDetailsProcessor(IConfiguration config)
         {
             _config = config;
             filmlerUrl = _config.GetSection("TheMovieAPI").GetSection("getMoviesPage").Value;
+            _dbContextOptions = DbContextOptionGenerator.BuildOptions(_config);
         }
 
         public void GetMovies(ref int page,ref int totalLastPage)
@@ -28,13 +30,19 @@ namespace DAL.Worker
                 MovieRestClient client = MovieRestClientFactory.GetNewMovieRestClient(filmlerUrl);
                 MovieRestRequest sorgulaReq = null;
 
-                using (var db = new MovieContext(new DbContextOptions<MovieContext>()))
+                using (var db = new MovieContext(_dbContextOptions))
                 {
-                    if (page != 1)
+                    var veriVarMi = db.MovieEntity;
+
+                    if (veriVarMi.Any())
                     {
-                        var lastpage = db.MovieEntity.LastOrDefault();
-                        page = lastpage == null ? 1 : lastpage.page+1;
+                        page = veriVarMi.LastOrDefault().page+1;
                     }
+                    else
+                    {
+                        page = 1;
+                    }
+                    
 
                     sorgulaReq = new MovieRestRequest();
                     sorgulaReq.AddParameter("page",page);
@@ -46,7 +54,7 @@ namespace DAL.Worker
                     //son sayfa film listesi çekildiğinde sayfa dolmamış ise ve yeni filmler gelmiş ise son sayfa silinecek ve yeniden eklenecek.
                     if (totalLastPage == sonuc.page && page != 1)
                     {
-                        var sonPage = db.MovieEntity.Where(w => w.page == sonuc.page).FirstOrDefault();
+                        var sonPage = db.MovieEntity.Include(inc=>inc.results).Where(w => w.page == sonuc.page).FirstOrDefault();
                         db.MovieEntity.Remove(sonPage);
                     }
 
